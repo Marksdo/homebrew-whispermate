46739bd95608d7ed931c0e19f8f5715089664d6d
Marksdo <imarksdo@gmail.com>
2026-05-16 14:14:43 +0800

    Update whispermate to 12.4

--- Casks/whispermate.rb
+++ Casks/whispermate.rb
@@ -1,6 +1,6 @@
 cask "whispermate" do
   version "12.4"
-  sha256 "ea8c77b2ef81e59566491d061d2da495ec281923047a25d214cf5374e9487bcb"
+  sha256 "dc712289441ec9ed9ea9ce5606f227dadde9f1e01af5460ada9312ca23de0180"
 
   url "https://github.com/Marksdo/homebrew-whispermate/releases/download/v#{version}/WhisperMateUniversal-#{version}.zip"
   name "Whisper Mate"
