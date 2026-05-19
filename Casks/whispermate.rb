b236dcd8975a91007948e1b01195f2895ec2975f
Marksdo <imarksdo@gmail.com>
2026-05-19 10:39:16 +0800

    Update whispermate to 12.5

--- Casks/whispermate.rb
+++ Casks/whispermate.rb
@@ -1,6 +1,6 @@
 cask "whispermate" do
-  version "12.5,597"
-  sha256 "0229756fc2014c885b9bc0b459de8ae002a37f2efb25e0b9f92c74cbe17b5e39"
+  version "12.5,598"
+  sha256 "5af5b1c9af51af2f5d8ab5d8e9e0bdd4c7e9493ae94d9b88ab3625dfaa677617"
 
   url "https://github.com/Marksdo/homebrew-whispermate/releases/download/v#{version.before_comma}/WhisperMateUniversal-#{version.before_comma}-#{version.after_comma}.zip"
   name "Whisper Mate"
@@ -10,7 +10,7 @@ cask "whispermate" do
   depends_on macos: ">= :ventura"
 
   app "Whisper Mate Universal.app"
-  binary "#{appdir}/Whisper Mate Universal.app/Contents/MacOS/Whisper Mate Universal", target: "whispermate"
+  binary "#{appdir}/Whisper Mate Universal.app/Contents/MacOS/whispermate-cli", target: "whispermate"
 
   zap trash: [
     "~/Library/Application Support/com.marksdo.WhisperMate",
