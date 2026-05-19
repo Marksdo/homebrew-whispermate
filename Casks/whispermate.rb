cbca49ad2acec561c00947cf6df3c14bef961179
Marksdo <imarksdo@gmail.com>
2026-05-19 13:23:45 +0800

    Update whispermate to 12.5

--- Casks/whispermate.rb
+++ Casks/whispermate.rb
@@ -1,6 +1,6 @@
 cask "whispermate" do
-  version "12.5,598"
-  sha256 "5af5b1c9af51af2f5d8ab5d8e9e0bdd4c7e9493ae94d9b88ab3625dfaa677617"
+  version "12.5,599"
+  sha256 "9c06d05a4e41115ceebc8de278e50c186c7fa7506ee760264a61e125e16c2d5c"
 
   url "https://github.com/Marksdo/homebrew-whispermate/releases/download/v#{version.before_comma}/WhisperMateUniversal-#{version.before_comma}-#{version.after_comma}.zip"
   name "Whisper Mate"
@@ -10,11 +10,10 @@ cask "whispermate" do
   depends_on macos: ">= :ventura"
 
   app "Whisper Mate Universal.app"
-  binary "#{appdir}/Whisper Mate Universal.app/Contents/MacOS/whispermate-cli", target: "whispermate"
+  binary "#{appdir}/Whisper Mate Universal.app/Contents/Resources/whispermate-cli", target: "whispermate"
 
   zap trash: [
     "~/Library/Application Support/com.marksdo.WhisperMate",
-    "~/Library/Application Support/whispermate",
     "~/Library/Preferences/com.marksdo.WhisperMate.plist",
     "~/Library/Caches/com.marksdo.WhisperMate",
     "~/Library/Saved Application State/com.marksdo.WhisperMate.savedState",
